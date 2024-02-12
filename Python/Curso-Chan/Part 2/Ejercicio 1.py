db6456afa5e60b65bd960f066abe945845c5ed5c
Aldo samuel vladimir Quintero <danolexpro08@gmail.com>
2024-02-12 20:48:07 -0300

    se volvio uwu

---
+++ Python/Curso-Chan/Part 2/Ejercicio 1.py
@@ -0,0 +1,8 @@
+"""Hacer un programa para mostrar los números del 1 al 10. No se debe realizar
+ningún pedido de datos. USANDO WHILE."""
+
+numero = 1
+
+while numero <= 10:
+    print(numero)
+    numero += 1
